8f1af49830f5a64883133f74d2f12d8a46c48652
Aditya Singh <83604707+adityarathore063@users.noreply.github.com>
2023-07-15 21:23:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,14 +44,6 @@ const Header = () => {
               >
                 Login
               </Button>
-              // <Button
-              //   LinkComponent={Link}
-              //   to="/auth"
-              //   variant="contained"
-              //   sx={{ margin: 1, borderRadius: 10 }}
-              // >
-              //   Signup
-              // </Button>
             </>
           )}
           {isLoggedIn && (
